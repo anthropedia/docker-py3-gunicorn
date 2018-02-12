685d80a1beedb85ad7c54967c631d9d34c361150
Vincent Agnano <vincent.agnano+github@scopyleft.fr>
2018-02-11 19:00:15 -0600

    added gunicorn timeout and log to console

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ -e requirements.txt ]; then
 	yes w | pip3 install -r requirements.txt
 fi
 
-gunicorn app -b :80 --workers 2 --name app
+gunicorn app -b :80 --workers 3 --name app --timeout 600 --log-file=-
